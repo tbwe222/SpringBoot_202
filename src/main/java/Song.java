d181b31adc4b1934a0764087d4ad508cb35fa0a3
GBTC408017UR <GBTC408017UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-15 10:58:10 -0400

    first commit

---
+++ src/main/java/Song.java
@@ -0,0 +1,16 @@
+public class Song {
+    private long id;
+    private String name;
+    private String artist;
+    private String album;
+    private int rating;
+    private int year;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
